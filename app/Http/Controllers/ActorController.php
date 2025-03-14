5e411c4d24807d19de19a610a737b063b08ca460
EricB-59 <ebaenac59@gmail.com>
2025-03-14 19:15:27 +0100

    Films done

---
+++ app/Http/Controllers/ActorController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class ActorController extends Controller
+{
+    /**
+     * Read Actors from storage
+     */
+    public static function readActor(): array
+    {
+        return [];
+    }
+}
